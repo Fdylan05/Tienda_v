bf6b5641ba9309c787b2700645efcfd555be9384
Dylan <Dylan@DESKTOP-N2NGT2F>
2022-03-07 17:57:54 -0600

    Actualizacion Semana 6 Controlador de persona completo Actualizacion de Repositorio de Persona completo Creacion de interfaz para actualizar personas y tener una vista de ellos

---
+++ src/main/java/com/tienda/controller/PersonasController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.controller;
+
+import com.tienda.entity.Persona;
+import com.tienda.service.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PersonasController {
+
+    @Autowired
+    private IPersonaService personaService;
+
+    @GetMapping("/personas")
+    public String index(Model model) {
+        List<Persona> listaPersonas = personaService.getAllPerson();
+        //System.out.print(listaPersonas);
+        model.addAttribute("titulo", "Personas");
+        model.addAttribute("personas", listaPersonas);
+        return "personas";
+    }
+    
+    @GetMapping("/personasN")
+    public String crearPersona(Model model){
+        model.addAttribute("persona", new Persona());
+        return "crear";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+        personaService.savePerson(persona);
+        return "redirect:/personas";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id") Long idPersona){
+        personaService.delete(idPersona);
+        return "redirect:/personas";
+    }
+    
+    
+}
